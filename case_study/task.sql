ff9b98bddb84f9fb1b56f4cc24bb94c22c1e6e7c
nguyenanhtam2608 <nguyenanhtam12344321@gmail.com>
2021-11-19 17:28:22 +0700

    task15

--- case_study/task.sql
+++ case_study/task.sql
@@ -100,6 +100,20 @@ where(h.ngay_lam_hop_dong between '2019-01-01' and '2019-12-31') and  h.ma_dich_
 select distinct ho_ten
 from khach_hang;
 
+select ho_ten 
+from khach_hang
+group by ho_ten;
+
+select ho_ten
+from khach_hang 
+union 
+select ho_ten 
+from khach_hang;
+
+
+
+
+
 -- task 9 
 -- Thực hiện thống kê doanh thu theo tháng, nghĩa là tương ứng với mỗi tháng 
 -- trong năm 2019 thì sẽ có bao nhiêu khách hàng thực hiện đặt phòng.
@@ -136,6 +150,97 @@ inner join hop_dong_chi_tiet hd on h.ma_hop_dong = hd.ma_hop_dong
   inner join loai_khach lk on kh.ma_loai_khach = lk.ma_loai_khach
   where lk.ten_loai_khach = 'diamond' and (kh.dia_chi ='vinh' or kh.dia_chi ='dn');
  
+ 
+ -- task 12
+		 select h.ma_hop_dong , nv.ho_ten as ho_ten_nhan_vien, kh.ho_ten,kh.sdt , dv.ten_dich_vu ,sum(v.don_vi) as so_luong_dich_vu_di_kem ,h.tien_dat_coc
+		 from hop_dong h
+		 inner join hop_dong_chi_tiet hd on h.ma_hop_dong = hd.ma_hop_dong
+		 inner join dich_vu_di_kem v on hd.ma_dich_vu_di_kem = v.ma_dich_vu_di_kem
+		 inner join khach_hang kh on h.ma_khach_hang = kh.ma_khach_hang
+		 inner join loai_khach lk on kh.ma_loai_khach = lk.ma_loai_khach
+		 inner join nhan_vien nv on h.ma_nhan_vien = nv.ma_nhan_vien
+         inner join dich_vu dv on h.ma_dich_vu = dv.ma_dich_vu
+         where (h.ngay_lam_hop_dong between '2019-10-01' and '2019-12-01') and h.ma_hop_dong = hd.ma_hop_dong
+                  group by h.ma_hop_dong
+         and not exists(
+         select h.ma_hop_dong , nv.ho_ten as ho_ten_nhan_vien, kh.ho_ten,kh.sdt , dv.ten_dich_vu ,sum(v.don_vi) as so_luong_dich_vu_di_kem ,h.tien_dat_coc
+		 from hop_dong h
+		 inner join hop_dong_chi_tiet hd on h.ma_hop_dong = hd.ma_hop_dong
+		 inner join dich_vu_di_kem v on hd.ma_dich_vu_di_kem = v.ma_dich_vu_di_kem
+		 inner join khach_hang kh on h.ma_khach_hang = kh.ma_khach_hang
+		 inner join loai_khach lk on kh.ma_loai_khach = lk.ma_loai_khach
+		 inner join nhan_vien nv on h.ma_nhan_vien = nv.ma_nhan_vien
+         inner join dich_vu dv on h.ma_dich_vu = dv.ma_dich_vu
+         where (h.ngay_lam_hop_dong between '2019-01-01' and '2019-06-01') and h.ma_hop_dong = hd.ma_hop_dong
+            group by h.ma_hop_dong
+            );
+		
+-- task 13
+select v.ten_dich_vu_di_kem ,count(v.ma_dich_vu_di_kem) as so_lan
+  from hop_dong h
+		 inner join hop_dong_chi_tiet hd on h.ma_hop_dong = hd.ma_hop_dong
+		 inner join dich_vu_di_kem v on hd.ma_dich_vu_di_kem = v.ma_dich_vu_di_kem
+         group by v.ten_dich_vu_di_kem 
+         order by so_lan desc
+         limit 1;
+         
+         
+-- task 14
+select h.ma_hop_dong, l.ten_loai_dich_vu, v.ten_dich_vu_di_kem ,count(v.ma_dich_vu_di_kem) as so_lan_su_dung
+  from hop_dong h
+		 inner join hop_dong_chi_tiet hd on h.ma_hop_dong = hd.ma_hop_dong
+		 inner join dich_vu_di_kem v on hd.ma_dich_vu_di_kem = v.ma_dich_vu_di_kem
+         inner join dich_vu dv on h.ma_dich_vu = dv.ma_dich_vu
+         inner join loai_dich_vu l on 	dv.ma_loai_dich_vu = l.ma_loai_dich_vu
+		 group by v.ten_dich_vu_di_kem 
+         having so_lan_su_dung = 1;
+         
+         
+-- task 15 
+select n.ma_nhan_vien ,n.ho_ten,t.ten_trinh_do,b.ten_bo_phan,n.sdt,n.dia_chi ,count(h.ma_nhan_vien) as so_luong_lam_hop_dong
+from nhan_vien n
+inner join vi_tri v on n.ma_vi_tri = v.ma_vi_tri
+inner join trinh_do t on n.ma_trinh_do = t.ma_trinh_do
+inner join bo_phan b on n.ma_bo_Phan = b.ma_bo_phan
+inner join hop_dong h on n.ma_nhan_vien = h.ma_nhan_vien 
+where h.ngay_lam_hop_dong between '2018-01-01' and '2019-01-01'
+group by h.ma_nhan_vien
+having so_luong_lam_hop_dong <=3;
+
+-- task 16
+         
+
+select n.ma_nhan_vien ,n.ho_ten,t.ten_trinh_do,b.ten_bo_phan,n.sdt,n.dia_chi ,count(h.ma_nhan_vien) as so_luong_lam_hop_dong
+from nhan_vien n
+inner join vi_tri v on n.ma_vi_tri = v.ma_vi_tri
+inner join trinh_do t on n.ma_trinh_do = t.ma_trinh_do
+inner join bo_phan b on n.ma_bo_Phan = b.ma_bo_phan
+inner join hop_dong h on n.ma_nhan_vien = h.ma_nhan_vien 
+where h.ngay_lam_hop_dong between '2017-01-01' and '2019-01-01'
+group by h.ma_nhan_vien
+having so_luong_lam_hop_dong =0  ;
+
+
+SET FOREIGN_KEY_CHECKS=0;-- vô hiệu hóa khóa ngoại
+delete s.*
+from nhan_vien n 
+inner join vi_tri v on n.ma_vi_tri = v.ma_vi_tri
+inner join trinh_do t on n.ma_trinh_do = t.ma_trinh_do
+inner join bo_phan b on n.ma_bo_Phan = b.ma_bo_phan
+inner join hop_dong h on n.ma_nhan_vien = h.ma_nhan_vien 
+
+
+
+        
+
+		
+
+         
+         
+         
+		
+
+
 
 -- task 20 
 -- Hiển thị thông tin của tất cả các nhân viên và khách hàng có trong hệ thống,
